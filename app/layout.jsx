77d3e2caf4d77de3d885ca0b88a0cb059b4fa586
AliSherTR <94902748+AliSherTR@users.noreply.github.com>
2024-08-05 11:09:47 +0500

    responsive: header,main,

--- app/layout.jsx
+++ app/layout.jsx
@@ -21,9 +21,12 @@ export default function RootLayout({ children }) {
                         enableSystem
                         disableTransitionOnChange
                     >
+                        <div className=" lg:col-start-1 lg:col-end-1 col-start-1 col-span-full">
                         <SideBar />
 
-                        <main className="col-start-2 bg-[#f8f8fb] dark:bg-[#141625] col-span-full min-h-screen flex items-center justify-center overflow-auto ">
+                        </div>
+
+                        <main className="lg:col-start-2 bg-[#f8f8fb] dark:bg-[#141625] col-span-full min-h-screen flex items-center justify-center overflow-auto ">
                             <div className=" max-w-[1200px] m-auto w-full">
                                 {children}
                             </div>
--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -6,12 +6,14 @@ import avatar from "@/public/image-avatar.jpg";
 
 export default function SideBar() {
     return (
-        <aside className="lg:h-screen bg-[#252945] relative z-50 rounded-tr-[30px] rounded-br-[30px] border-none flex flex-col overflow-hidden">
+        <aside className="lg:h-screen  bg-transparent relative z-50 rounded-tr-[30px] rounded-br-[30px] border-none flex lg:flex-col overflow-hidden">
             <Logo />
-            <div className=" flex-1 flex flex-col justify-end items-center pb-10 border-b">
+
+            <div className=" flex lg:flex-col lg:w-full lg:items-center lg:justify-center flex-1 items-end justify-end gap-x-5 me-4 lg:me-0">
+            <div className=" lg:flex-1 flex flex-col justify-end items-center pb-10 lg:border-b lg:h-full lg:w-full">
                 <ThemeToggleButton />
             </div>
-            <div className=" py-10 flex items-center justify-center cursor-pointer">
+            <div className=" py-10 flex items-center justify-center cursor-pointer lg:w-full lg:self-stretch ">
                 <Image
                     src={avatar}
                     alt="avatar"
@@ -20,6 +22,8 @@ export default function SideBar() {
                     height={40}
                 />
             </div>
+            </div>
+           
         </aside>
     );
 }
